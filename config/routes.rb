9a76bcbb81edfb5b10d01769c08418adb9522a3a
SofeiAndreiGolfGenius <andrei.sofei@golfgenius.com>
2022-08-09 18:20:13 +0300

    Added home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
+  get 'user/new'
+  get 'user/show'
+  get 'user/edit'
+  get 'user/destroy'
+  root 'static_pages#home'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
