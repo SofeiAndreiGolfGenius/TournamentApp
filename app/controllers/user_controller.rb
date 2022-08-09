7c878649c72a387a32063db8d26434be05c3ca39
SofeiAndreiGolfGenius <andrei.sofei@golfgenius.com>
2022-08-09 18:23:25 +0300

    Fixed some rubocop errors

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,13 +1,9 @@
 class UserController < ApplicationController
-  def new
-  end
+  def new; end
 
-  def show
-  end
+  def show; end
 
-  def edit
-  end
+  def edit; end
 
-  def destroy
-  end
+  def destroy; end
 end
