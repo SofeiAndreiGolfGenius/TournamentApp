28196e907dc570a0f0334884149c9e6862c7fb7f
SofeiAndreiGolfGenius <andrei.sofei@golfgenius.com>
2022-08-12 14:56:40 +0300

    Added teams do DB with member relationship

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_09_153310) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_12_115418) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "teams", force: :cascade do |t|
+    t.string "name"
+    t.bigint "leader_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["leader_id"], name: "index_teams_on_leader_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -21,6 +29,10 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_09_153310) do
     t.datetime "updated_at", null: false
     t.string "password_digest"
     t.boolean "admin"
+    t.bigint "team_id"
+    t.index ["team_id"], name: "index_users_on_team_id"
   end
 
+  add_foreign_key "teams", "users", column: "leader_id"
+  add_foreign_key "users", "teams"
 end
